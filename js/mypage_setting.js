56f8ea33589c1031bc2184e6abc6a70b4157179c
nakyeong <yunna3425@naver.com>
2024-07-04 22:50:02 +0900

    feat: 마이페이지 모달창 구현

--- js/mypage_setting.js
+++ js/mypage_setting.js
@@ -1,3 +1,4 @@
+// 알림 동의 or 비동의
 document.getElementById('settingToggleContainer').addEventListener('click', function () {
     let settingToggleContainer = this;
     let toggleButton = settingToggleContainer.querySelector('.setting-toggle-btn');
@@ -10,4 +11,43 @@ document.getElementById('settingToggleContainer').addEventListener('click', func
         toggleButton.textContent = '동의';
         toggleButton.style.left = '-2px';
     }
+});
+
+// 비밀번호 동일한지 확인
+document.addEventListener('DOMContentLoaded', () => {
+    const accountDeleteBtn = document.getElementById('accout-delete-btn');
+    const modal = document.getElementById('password-alert');
+    const closeModalBtn = document.getElementById('password-alert-btn');
+
+    accountDeleteBtn.addEventListener('click', () => {
+        modal.style.display = 'block';
+    });
+
+    closeModalBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    const newPasswordInput = document.getElementById('new-password');
+    const confirmPasswordInput = document.getElementById('confirm-password');
+    const correctMessage = document.querySelector('.correct-message');
+    const errorMessage = document.querySelector('.error-message');
+
+    confirmPasswordInput.addEventListener('input', () => {
+        if (newPasswordInput.value === confirmPasswordInput.value) {
+            correctMessage.style.display = 'block';
+            errorMessage.style.display = 'none';
+        } else {
+            correctMessage.style.display = 'none';
+            errorMessage.style.display = 'block';
+        }
+    });
+});
+
+// 모달
+document.getElementById('accout-delete-btn').addEventListener('click', function() {
+    document.getElementById('password-alert').style.display = 'block';
+});
+
+document.getElementById('password-alert-btn').addEventListener('click', function() {
+    document.getElementById('password-alert').style.display = 'none';
 });
